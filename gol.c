d5ab14979887f0c5d3617c14dabd052264888044
kovani <kovani84@gmail.com>
2024-03-07 10:01:10 -0500

    some fixes and make more space

--- gol.c
+++ gol.c
@@ -3,8 +3,8 @@
 #include <time.h>
 #include <unistd.h>
 
-#define SIZE_Y 25
-#define SIZE_X 80
+#define SIZE_Y 50
+#define SIZE_X 160
 
 char board[SIZE_Y][SIZE_X];
 char next_board[SIZE_Y][SIZE_X];
@@ -14,7 +14,7 @@ void init_board() {
         for (int j = 0; j < SIZE_X; j++) {
             char state;
             scanf(" %c", &state);
-            if (state == '$' || state == '_') {
+            if (state == '$' || state == '.') {
                 board[i][j] = state;
             } else {
                 printf("n/a");
@@ -25,10 +25,11 @@ void init_board() {
 }
 
 void print_board() {
+    usleep(100000);
     system("clear");
     for (int i = 0; i < SIZE_Y; i++) {
         for (int j = 0; j < SIZE_X; j++) {
-            printf("%c ", board[i][j]);
+            printf("%c", board[i][j]);
         }
         printf("\n");
     }
@@ -36,27 +37,30 @@ void print_board() {
 }
 
 void update_board() {
-    usleep(500000);
     for (int i = 0; i < SIZE_Y; i++) {
         for (int j = 0; j < SIZE_X; j++) {
             int sosed = 0;
 
             for (int y = -1; y <= 1; y++) {
                 for (int x = -1; x <= 1; x++) {
-                    if (y == 0 && x == 0) continue;
+                    //if (y == 0 && x == 0) continue;
 
                     int sosed_Y = i + y;
                     int sosed_X = j + x;
 
-                    if (sosed_Y >= 0 && sosed_Y < SIZE_Y && sosed_X >= 0 && sosed_X < SIZE_X &&
-                        board[sosed_Y][sosed_X] == '$')
+                    if (sosed_Y == -1) sosed_Y = SIZE_Y-1;
+                    if (sosed_X == -1) sosed_X = SIZE_X-1;
+                    if (sosed_Y == SIZE_Y) sosed_Y = 0;
+                    if (sosed_X == SIZE_X) sosed_X = 0;
+
+                    if (board[sosed_Y][sosed_X] == '$')
                         sosed++;
                 }
             }
 
-            if (board[i][j] == '$' && (sosed < 2 || sosed > 3))
-                next_board[i][j] = '_';
-            else if (board[i][j] == '_' && sosed == 3)
+            if (board[i][j] == '$' && (sosed < 2 || sosed > 4))
+                next_board[i][j] = '.';
+            else if (board[i][j] == '.' && sosed == 3)
                 next_board[i][j] = '$';
             else
                 next_board[i][j] = board[i][j];
